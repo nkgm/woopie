6c69a20bc8754e4c5f08153c1bf126f808a49c15
nkgm <nkgm.realm@gmail.com>
2015-10-20 19:09:46 +0300

    Godspeed

--- install.sh
+++ install.sh
@@ -1,10 +1,88 @@
 #!/bin/bash
 
-SEARCHREPLACEDB=$(cat searchreplacedb.php)
+WOOPIE_RAW_REPO="https://raw.githubusercontent.com/skwp/dotfiles"
 
-read -r -d '' TEMPLATE <<- 'TEMPLATE'
+woopie_partial () {
+  PARTIAL=$1
+  if [[ -e $PARTIAL ]]; then
+    cat "$PARTIAL"
+  else
+    curl -fsSL "$WOOPIE_RAW_REPO/master/$PARTIAL"
+  fi
+}
+
+SEARCHREPLACEDB="$(php -w searchreplacedb.php | perl -p -e 's/^\s*<\?php\s*//s' | perl -p -e 's/\s*\?>\s*$//s')"
+
+read -r -d '' TEMPLATE << TEMPLATE
+#!/bin/bash
+
+read -r -d '' SEARCHREPLACEDB_PHP<<'EOF'
 $SEARCHREPLACEDB
+EOF
+
+woopie_searchreplacedb () {
+  php -r "\$SEARCHREPLACEDB_PHP" "\$@"
+}
+
+###########################################################
+#################### start _woopie-env ####################
+###########################################################
+
+$(woopie_partial _woopie-env)
+
+###########################################################
+##################### end _woopie-env #####################
+###########################################################
+
+###########################################################
+#################### start _woopie-db #####################
+###########################################################
+
+$(woopie_partial _woopie-db)
+
+###########################################################
+##################### end _woopie-db ######################
+###########################################################
+
+###########################################################
+################### start _woopie-apache ##################
+###########################################################
+
+$(woopie_partial _woopie-apache)
+
+###########################################################
+#################### end _woopie-apache ###################
+###########################################################
+
+###########################################################
+##################### start _woopie #######################
+###########################################################
+
+$(woopie_partial _woopie)
+
+###########################################################
+###################### end _woopie ########################
+###########################################################
+
 TEMPLATE
 
-echo $TEMPLATE > woopie
+WOOPIE_REMOTE=$(lsb_release -a 2>/dev/null | grep -iq ubuntu && echo true)
+
+if [[ -n $WOOPIE_REMOTE ]]; then
+  if [[ $(id -u) -ne 0 ]]; then
+    echo "woopie requires sudo to run or install on a remote - exiting"
+    exit 1
+  fi
+fi
+
+INSTALL_PATH="${1:-/usr/local/bin}/woopie"
+echo "Installing to $INSTALL_PATH"
+
+echo "$TEMPLATE" > "$INSTALL_PATH"
+
+if [[ -n $WOOPIE_REMOTE ]]; then
+  chmod 700 "$INSTALL_PATH"
+else
+  chmod +x "$INSTALL_PATH"
+fi
 
